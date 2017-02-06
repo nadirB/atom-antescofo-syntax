d4791e332968a569ee6831e056ff7340b2f6f1a4
Poncelet <clement.poncelet@ircam.fr>
2017-02-06 18:57:58 +0100

    config deps

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 module.exports = {
   activate() {
-    // replace the example argument 'linter-ruby' with the name of this Atom package
-    require('atom-package-deps').install('atom-antescofo')
+    require('atom-package-deps').install('atom-antescofo-syntax')
       .then(function() {
         console.log('All dependencies installed, good to go')
       })
